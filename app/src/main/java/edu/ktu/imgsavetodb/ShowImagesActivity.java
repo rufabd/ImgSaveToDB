a9c386284d0ee084794c30546d929bf89ab25d30
rufat <mrrufatabd@gmail.com>
2022-01-27 10:05:00 +0200

    Initial commit

---
+++ app/src/main/java/edu/ktu/imgsavetodb/ShowImagesActivity.java
@@ -0,0 +1,54 @@
+package edu.ktu.imgsavetodb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class ShowImagesActivity extends AppCompatActivity {
+
+    DatabaseHandler objectDatabaseHandler;
+    RecyclerView objectRecyclerView;
+    private RVAdapter objectRVAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_images);
+
+        try{
+            objectRecyclerView = findViewById(R.id.imagesRV);
+            objectDatabaseHandler = new DatabaseHandler(this);
+        } catch (Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void getData(View view) {
+        try {
+            objectRVAdapter = new RVAdapter(objectDatabaseHandler.getAllImagesData());
+            objectRecyclerView.setHasFixedSize(true);
+
+            objectRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+            objectRecyclerView.setAdapter(objectRVAdapter);
+        } catch (Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
